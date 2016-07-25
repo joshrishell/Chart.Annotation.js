d147fac728d49ed0970936c55086fa35d19cfab4
Josh Rishell <jrishell@hlkagency.com>
2016-07-25 13:14:43 -0400

    adding options to allow for border dash and maximum values for line annotations

--- src/line.js
+++ src/line.js
@@ -12,11 +12,15 @@ module.exports = function(Chart) {
 			ctx.lineWidth = view.borderWidth;
 			ctx.strokeStyle = view.borderColor;
 
+			if (view.borderSetDash) {
+				ctx.setLineDash(view.borderSetDash);
+			}
 			// Draw
 			ctx.beginPath();
 			ctx.moveTo(view.x1, view.y1);
 			ctx.lineTo(view.x2, view.y2);
 			ctx.stroke();
+			ctx.setLineDash([]);
 		}
 	});
 
@@ -24,16 +28,18 @@ module.exports = function(Chart) {
 		var model = obj._model = obj._model || {};
 
 		var scale = chartInstance.scales[options.scaleID];
+		var scaleMax = chartInstance.scales[options.maxScaleID];
 		var pixel = scale ? scale.getPixelForValue(options.value) : NaN;
+		var pixelMax = scale ? scaleMax.getPixelForValue(options.maxValue) : NaN;
 		var chartArea = chartInstance.chartArea;
 
 		if (!isNaN(pixel)) {
 			if (options.mode == horizontalKeyword) {
 				model.x1 = chartArea.left;
-				model.x2 = chartArea.right;
+				model.x2 = pixelMax || chartArea.right;
 				model.y1 = model.y2 = pixel;
 			} else {
-				model.y1 = chartArea.top;
+				model.y1 = pixelMax || chartArea.top;
 				model.y2 = chartArea.bottom;
 				model.x1 = model.x2 = pixel;
 			}
@@ -41,6 +47,7 @@ module.exports = function(Chart) {
 
 		model.borderColor = options.borderColor;
 		model.borderWidth = options.borderWidth;
+		model.borderSetDash = options.borderSetDash;
 	}
 
 
